e2bd14e8faaebb5c20c3a0ed198a87e6f83123fc
mimipeshy <perisndanu@gmail.com>
2024-02-01 16:16:44 +0300

    first commit

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <p className="copyright">&copy; 2023 - 2024 www.perisndanu.co.ke - All Rights Reserved.</p> 
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./main.css"
+
+function Main() {
+    return (
+      <div className="main-fact">
+                 <h1>Fun Facts About Me</h1>
+                 <ol>
+                    <li>Age:23 years old.</li>
+                    <li>Sign : Leo</li>
+                    <li>Favorite food: Rice</li>
+                 </ol>
+      </div>
+    )
+}
+export default Main;
